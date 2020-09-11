618c1cad1c0a8c6968a4a8d65807587247ed65ad
Benjamin Legendre <legendre.benjamin@gmail.com>
2020-09-11 16:56:19 +0200

    Finished first version of m3uragen (works but ugly)

--- m3uragen.py
+++ m3uragen.py
@@ -3,35 +3,20 @@
 from pathlib import Path
 import sys
 import re
-
-# Process arguments
-
-if len(sys.argv) < 3:
-    sys.stderr('ERROR: bad number of arguments')
-    sys.exit(-1)
-
-coldir = Path(sys.argv[1]).resolve()
-m3udir = Path(sys.argv[2]).resolve()
-patterns = sys.argv[3:]
-
-# Iter sub folders and folder as final folder
-#
-# Filter list: items that contains the regex
-# Sort list
-#
-# for each item
-#   (refer to algo in readme)
-
-if not coldir.is_dir():
-    sys.stderr.write(f'ERROR: Directory does not exists: {coldir}\n')
-    sys.exit(-1)
+import os
 
 
-def createm3u(imgset):
-    print(imgset)
+def create_m3u(outpath, name, files):
+    m3ufile = str(outpath) + '/' + name + '.m3u'
+    print(f'Write: {m3ufile}')
+    resource = open(m3ufile, 'w')
+    for i in files:
+        imgpath = os.path.relpath(i, outpath)
+        resource.write(imgpath + '\n')
+    resource.close()
 
 
-def process_dir(dir):
+def process_dir(dir, m3udir):
     print(f'Process directory: {dir}')
     imgfiles = []
     for path in dir.iterdir():
@@ -48,20 +33,37 @@ def process_dir(dir):
             multiparts_str = match.group(1)
             img_appname = imgfile.replace(multiparts_str, '')
             if img_appname != cur_appname and len(appimgset) > 0:
-                createm3u(appimgset)
+                name = Path(cur_appname).name
+                create_m3u(m3udir, name, appimgset)
                 appimgset.clear()
-
             appimgset.append(imgfile)
             cur_appname = img_appname
 
 
-def scan_dirs(dir):
+def scan_dirs(dir, m3udir):
 
     for path in dir.iterdir():
         if path.is_dir():
-            scan_dirs(path)
-            process_dir(path)
+            scan_dirs(path, m3udir)
+            process_dir(path, m3udir)
 
 
-scan_dirs(coldir)
+# Process arguments
 
+if len(sys.argv) < 3:
+    sys.stderr('ERROR: bad number of arguments')
+    sys.exit(-1)
+
+# Check and prepare directories
+
+coldir = Path(sys.argv[1]).resolve()
+m3udir = Path(sys.argv[2]).resolve()
+patterns = sys.argv[3:]
+
+if not coldir.is_dir():
+    sys.stderr.write(f'ERROR: Directory does not exists: {coldir}\n')
+    sys.exit(-1)
+
+m3udir.mkdir(parents=True)    
+
+scan_dirs(coldir, m3udir)
