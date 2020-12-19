f7f76d45fe71ed068822f4853b249149fabe2668
Benjamin Legendre <legendre.benjamin@gmail.com>
2020-12-19 13:03:29 +0100

    Setup pip packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="m3uragen",
+    version="1.0.2",
+    author="Benjamin Legendre",
+    author_email="jeff58888@hotmail.com",
+    description="Generate M3U playlists from multi images software romsets for use with RetroArch ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TiBeN/m3uragen",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': [
+            'm3uragen=m3uragen.main:_main'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
